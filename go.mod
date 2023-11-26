036485559362c01740a93cdd8212175a49cd8beb
吴小白 <296015668@qq.com>
2023-11-26 11:47:21 +0800

    tcp: fix build on linux/loong64, linux/riscv64

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/marten-seemann/tcp
+module github.com/wojiushixiaobai/tcp
 
 go 1.15
 
@@ -6,5 +6,5 @@ require (
 	github.com/mikioh/tcp v0.0.0-20190314235350-803a9b46060c
 	github.com/mikioh/tcpinfo v0.0.0-20190314235526-30a79bb1804b
 	github.com/mikioh/tcpopt v0.0.0-20190314235656-172688c1accc
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
+	golang.org/x/net v0.18.0
 )
--- info_test.go
+++ info_test.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build darwin || freebsd || linux || netbsd
 // +build darwin freebsd linux netbsd
 
 package tcp_test
@@ -18,8 +19,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/mikioh/tcp"
 	"github.com/mikioh/tcpinfo"
+	"github.com/wojiushixiaobai/tcp"
 )
 
 var infoTests = []struct {
